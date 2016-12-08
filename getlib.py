0d897b8bd787ad8b4aa75c4dfcd8f057bbbd5b9a
rendiya <ligerrendy@gmail.com>
2016-12-08 09:12:04 +0700

    update check

--- getlib.py
+++ getlib.py
@@ -1,6 +1,6 @@
 import urllib2
 import json
-
+import random
 def get_url(url="https://api.github.com/orgs/openedoo/repos"):
 	response = urllib2.urlopen(url)
 	data = json.load(response)
@@ -28,26 +28,29 @@ for number_awal in xrange(number_awal,number_akhir):
 	else:
 		print "false"
 '''
-def check_modul(url=None):
+def check_modul_avaible(url=None):
 	data = get_url()
 	number_git = len(data)
 	number_awal = 0
+	list_all = []
 	for number_awal in xrange(number_awal,number_git):
 		jumlah = (number_awal+1)-1
 		modul = data[jumlah]
 		cek_modul = data[jumlah]['name']
 		if 'modul' in cek_modul:
-			list_name = []
-			list_name.append(data[jumlah]['name'])
-			print "true"
-			return list_name
-			url = modul['contents_url']
-			url_modul = url.replace("{+path}", "list_modul.json")
-			try:
-				data2 = get_url(url_modul)
-				print data2
-			except Exception as error:
-				print error
+			modul_url = modul['contents_url']
+			modul_url = modul_url.replace("{+path}", "requirement.json")
+			modul_name = modul['name']
+			modul_list = {'name':modul_name,'url':modul_url}
+			list_all.append(modul_list)
 		else:
-			print "false"
-print check_modul()
+			pass
+	return list_all
+def autoSolve():
+	y = []
+	for i in range(5):
+		z = random.randrange(1, 10)
+		y.append(z)
+	print(y)
+#autoSolve()
+print check_modul_avaible()
