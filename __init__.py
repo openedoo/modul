d62fe73c663b361381907fa3247ec1ea0a21cea8
rendiya <ligerrendy@gmail.com>
2016-12-15 14:59:54 +0700

    hello moduls

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 from openedoo.core.libs import blueprint
 
 hello = blueprint('hello', __name__)
-
 @hello.route('/', methods=['POST', 'GET'])
 def index():
 	return "Hello World"
--- kompress.py
+++
@@ -1,5 +0,0 @@
-import tarfile
-tar = tarfile.open("sample.tar.gz", "w:gz")
-for name in ["module_hello"]:
-	tar.add(name)
-tar.close()
